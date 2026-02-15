2595c3f7db3553c0687cdd1551daa9bf99e3dabb
Edward Chu <somestyle@gmail.com>
2026-02-15 08:15:26 -0500

    update changelog

--- src/App.jsx
+++ src/App.jsx
@@ -429,7 +429,7 @@ const ThemeMenu = ({
             onClick={onChangelogClick}
             className="text-xs text-stone-400 dark:text-stone-500 hover:text-stone-600 dark:hover:text-stone-300 transition-colors"
           >
-            v1.2 (Changelog)
+            v1.0 (Changelog)
           </button>
         </div>
       )}
